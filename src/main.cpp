1e39f318b9f0bc8a828652eb6838ef355d144d7b
Saravana2611 <p.saravanan2611@gmail.com>
2024-03-24 15:06:26 +0530

    Added qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "mylib.hpp"
 
+#include <QApplication>
+#include <QPushButton>
+
 using namespace std;
 
-int main()
-{
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv); // Create the Qt application object
+
+    QPushButton button("Click me!"); // Create a QPushButton widget
+    button.resize(100, 30); // Set the size of the button
+    button.show(); // Display the button
+
     cout << mylib::add(10, 2) << endl;
 
     cout << mylib::add(10, 2) << endl;
@@ -16,4 +24,6 @@ int main()
     cout << mylib::sub(1.0, 2.5) << endl;
     cout << mylib::mul(1.0, 2.5) << endl;
     cout << mylib::div(1.0, 2.5) << endl;
+    
+    return app.exec(); // Enter the Qt event loop
 }
